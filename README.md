0fa9b7d31022e64737b45829c42dbf0f9129e66b
Wim <wim@sitebase.be>
2015-04-11 18:15:43 +0200

    basic implementation and preview

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# tinymce-variable
+# TinyMCE variable
+
 TinyMCE variable is a plugin that makes it easier to work with variables in text.
+
+## Why
+
+I see a lot of companies that have the need for variables in their editors. Maybe one of the most popular is [MailChimp](http://mailchimp.com/) ([merge tags](http://mailchimp.com/features/merge-tags/)), but non of these companies seems to solve this problem in a very usable way. Especially for non technical people I can imagine that seeing something like `*[FNAME|*` scares the sh*t out of them.
+
+## Features
+
+* Replace variables like `{example}` with something more visual
+* Variables are not editable
+* Delete variables with one hit on the backspace button
+
+## Develop
+
+Start a server to and open it in your browser:
+
+    python -m SimpleHTTPServer
---
+++ src/main.js
@@ -0,0 +1,192 @@
+/**
+ * plugin.js
+ *
+ * Copyright, Sitebase
+ * Released under MIT License.
+ *
+ * License: http://www.sitebase.be
+ * Contributing: http://www.sitebase.be/contributing
+ */
+
+/*global tinymce:true */
+
+tinymce.PluginManager.add('variables', function(editor) {
+
+    var VK = tinymce.util.VK;
+    var stringVariableRegex = new RegExp('{([a-z. _]*)?}', 'g');
+
+    // make the variables more readable by replacing them
+    // with another string
+    var mappers = {
+        username: 'Username',
+        email: 'Email',
+        widget_name: 'Widget name',
+        entry_id: 'Entry ID'
+    }
+
+    // let user define a list of valid variables
+    var valid_variables = {
+
+    };
+
+    /**
+     * convert variable strings into html elements
+     * @return {void}
+     */
+    function stringToHTML()
+    {
+        var nodeList = [],
+            nodeValue,
+            node,
+            div;
+
+        // find nodes that contain a string variable
+        tinymce.walk(editor.getBody(), function(n) {
+            if (n.nodeType == 3 && n.nodeValue && stringVariableRegex.test(n.nodeValue)) {
+                nodeList.push(n);
+            }
+        }, 'childNodes');
+
+        // loop over all nodes that contain a string variable
+        for (i = 0; i < nodeList.length; i++) {
+            nodeValue = nodeList[i].nodeValue.replace(stringVariableRegex, createHTMLVariable);
+            div = editor.dom.create('div', null, nodeValue);
+            while ((node = div.lastChild)) {
+                editor.dom.insertAfter(node, nodeList[i]);
+            }
+
+            // remove text variable node
+            // because we now have an HTML representation of the variable
+            editor.dom.remove(nodeList[i]);
+        }
+
+    }
+
+    /**
+     * convert a text variable "x" to a span with the needed
+     * attributes to style it with CSS
+     * @param  {string} value
+     * @return {string}
+     */
+    function createHTMLVariable( value ) {
+
+        var clean_value = value.replace(/[^a-zA-Z._]/g, "");
+
+        // map value to a more readable value
+        if( mappers.hasOwnProperty(clean_value) )
+            clean_value = mappers[clean_value];
+
+        editor.fire('VariableToHTML', {
+            value: value,
+            cleanValue: clean_value
+        });
+
+        return '<span class="variable" data-original-variable="' + value + '">' + clean_value + '</span>';
+    }
+
+    /**
+     * convert HTML variables back into their original string format
+     * for example when a user opens source view
+     * @return {void}
+     */
+    function HTMLToString()
+    {
+        var nodeList = [],
+            nodeValue,
+            node,
+            div;
+
+            // find nodes that contain a HTML variable
+        tinymce.walk( editor.getBody(), function(n) {
+            var original = n.parentElement.getAttribute('data-original-variable');
+            if (original !== null) {
+                nodeList.push(n);
+            }
+        }, 'childNodes');
+
+        // loop over all nodes that contain a HTML variable
+        for (i = 0; i < nodeList.length; i++) {
+            nodeValue = nodeList[i].parentElement.getAttribute('data-original-variable');
+            div = editor.dom.create('div', null, nodeValue);
+            while ((node = div.lastChild)) {
+                editor.dom.insertAfter(node, nodeList[i].parentElement);
+            }
+
+            // remove HTML variable node
+            // because we now have an text representation of the variable
+            editor.dom.remove(nodeList[i].parentElement);
+        }
+
+    }
+
+    /**
+     * get variable out of a string
+     * keep in mind that this will only return the first variable even if there are more then one
+     * for example "{hello} test {world}" will return "hello"
+     * @param  {string} value
+     * @return {string}
+     */
+    function getVariable(value) {
+        var variable, clean_variable;
+        var variable_pick_regex = new RegExp('{([a-z. _]*)?}', 'g');
+        var variable_clean_regex = new RegExp('[^a-zA-Z._]', 'g');
+        var matches = value.match( variable_pick_regex );
+        var result = {};
+
+        if( matches.length > 0 ) {
+            for( var i=0 ; i < matches.length ; i++ ) {
+                variable = matches[i];
+                clean_variable = variable.replace( variable_clean_regex, '');
+                result[ clean_variable ] = variable;
+            }
+            return result;
+        }
+
+        return null;
+    }
+
+    /**
+     * this function will make sure variable HTML elements can
+     * not be edited
+     * and also make it possible to delete them by hitting backspace
+     * @param  {object} e
+     * @return {void}
+     */
+    function editableHandler(e) {
+
+        var currentNode = tinymce.activeEditor.selection.getNode();
+        var keyCode = e.keyCode;
+
+        if( currentNode.classList.contains('variable') ) {
+
+            if( keyCode === VK.DELETE || keyCode === VK.BACKSPACE ) {
+                // user can delete variable nodes
+                editor.fire('VariableDelete', {value: currentNode.nodeValue});
+                editor.dom.remove( currentNode );
+            } else if( keyCode === VK.LEFT || keyCode === VK.RIGHT || keyCode === VK.TOP || keyCode === VK.BOTTOM ) {
+                // allow
+            } else {
+                // user can not modify variables
+                e.preventDefault();
+                editor.fire('VariableModifyAttempt', {node: currentNode});
+            }
+        }
+    }
+
+    /**
+     * handle formatting the content of the editor based on
+     * the current format. For example if a user switches to source view and back
+     * @param  {object} e
+     * @return {void}
+     */
+    function handleContentRerender(e) {
+        return e.format === 'raw' ? stringToHTML() : HTMLToString();
+    }
+
+
+    editor.on('nodechange', stringToHTML );
+    editor.on('keyup', stringToHTML );
+    editor.on('keydown', editableHandler );
+    editor.on('beforegetcontent', handleContentRerender);
+
+});
